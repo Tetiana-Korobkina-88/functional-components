a1e1c1e6a5c46f76feb14f54f38c667790bf4ba1
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-08 16:52:47 +0200

    created a new functional component and worked with life cycles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
+import Clicker from "./Clicker";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1)
-  }
-
-  const decrement = () => {
-    setCount(count - 1)
-  }
+  const [isClicker, setClicker] = useState(false);
 
   return (
     <div className="App">
-      <button onClick={decrement}>-</button>
-      <span style={{display: "inline-block", margin: '0 0.5rem'}}>{count}</span>
-      <button onClick={increment}>+</button>
+      <h2>React App</h2>
+      <button onClick={() => setClicker(!isClicker)}>Toggle clicker</button>
+      {isClicker && <Clicker />}
     </div> 
   );
 } 
