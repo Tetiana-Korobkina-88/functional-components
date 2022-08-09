d50568c4b23e15ce9afbf7efaf4619f47f2ec513
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-09 15:21:13 +0200

    created a timer through a functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Clicker from "./Clicker";
 import WithRef from "./Ref"
+import Timer from "./Timer";
 
 function App() {
 
   const [isClicker, setClicker] = useState(false);
+  const [isTimer, setTimer] = useState(false);
 
   return (
     <div className="App">
@@ -12,6 +14,8 @@ function App() {
       <button onClick={() => setClicker(!isClicker)}>Toggle clicker</button>
       {isClicker && <Clicker />}
       <WithRef />
+      <button onClick={() => setTimer(!isTimer)}>Toggle timer</button>
+      {isTimer && <Timer />}
     </div> 
   );
 } 
